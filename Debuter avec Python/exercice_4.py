440cb8f05e9eceb77c9f847b47dfb46392e735c5
Maya Divet <mayad@lyceechurchill.london>
2024-09-24 14:08:50 +0000

    questions6-8

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -2,10 +2,19 @@
 
 c = 3
 P = 4*c
-A = c
+A = c**2
 b = A>5
 
 #Question 6:
 
-def perimetre(x):
-    return 4*c
+def perimetre():
+    return(4*c)
+
+print(perimetre())
+
+#Question 7:
+
+def surface():
+    return(c**2)
+
+print(surface())
