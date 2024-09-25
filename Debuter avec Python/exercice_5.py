3ef54c45998e296b8849efdffdc10b25852f2eee
Maya Divet <mayad@lyceechurchill.london>
2024-09-25 18:31:43 +0000

    exo 5

---
+++ Debuter avec Python/exercice_5.py
@@ -0,0 +1,9 @@
+#Exercice 5
+#1. La formule est 2*pi*r
+#2. l'argument est le rayon du cercle, de type float.
+#3: Algorithme complete
+
+r=5
+def perimetre_cercle(r):
+    return(2*3.14*r)
+print(perimetre_cercle(r))
