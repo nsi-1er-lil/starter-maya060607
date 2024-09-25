075ada3eca701676919003c37d76b259e091a69b
Maya Divet <mayad@lyceechurchill.london>
2024-09-25 19:52:23 +0000

    commit

--- Debuter avec Python/exercice_6.py
+++ Debuter avec Python/exercice_6.py
@@ -4,14 +4,19 @@ from math import pi
 #2) On a r = rayon et h = hauteur, et les deux sont des float
 #3)
 
-R = 5
-H = 8
 def V_cylindre(r, h):
     return(pi*r**2*h)
-print(V_cylindre(r=R,h=H))
 
 def A_cylindre(r, h):
     return(2*pi*r**2+2*pi*r*h)
+
+R = 5
+H = 8
+print(V_cylindre(r=R,h=H))
 print(A_cylindre(r=R,h=H))
 
-print(A_cylindre(r=R,h=H))
+# if __name__ == "__main__":
+#    R = 5
+#    H = 8
+#    print(V_cylindre(r=R,h=H))
+#    print(A_cylindre(r=R,h=H))
