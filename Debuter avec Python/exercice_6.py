d5df17630322ff386bbacef4584a013d3441f6ec
Maya Divet <mayad@lyceechurchill.london>
2024-09-25 19:02:22 +0000

    commit exo 6

--- Debuter avec Python/exercice_6.py
+++ Debuter avec Python/exercice_6.py
@@ -1,14 +1,17 @@
+from math import pi
 #Exercice 6
 #1) V_cylindre = pi*r**2*h et A_cylindre = 2*pi*r**2 + 2*pi*r*h
 #2) On a r = rayon et h = hauteur, et les deux sont des float
 #3)
 
-r = 5
-h = 8
-def V_cylindre():
-    return(3.14*r**2*h)
-print(V_cylindre())
+R = 5
+H = 8
+def V_cylindre(r, h):
+    return(pi*r**2*h)
+print(V_cylindre(r=R,h=H))
 
-def A_cylindre():
-    return(2*3.14*r**2+2*3.14*r*h)
-print(A_cylindre())
+def A_cylindre(r, h):
+    return(2*pi*r**2+2*pi*r*h)
+print(A_cylindre(r=R,h=H))
+
+print(A_cylindre(r=R,h=H))
