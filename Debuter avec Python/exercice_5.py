b85a9139f801fb1149b870cce6bcbe86d989c03e
Maya Divet <mayad@lyceechurchill.london>
2024-09-25 18:51:46 +0000

    commit exo 5

--- Debuter avec Python/exercice_5.py
+++ Debuter avec Python/exercice_5.py
@@ -1,3 +1,4 @@
+import math
 #Exercice 5
 #1. La formule est 2*pi*r
 #2. l'argument est le rayon du cercle, de type float.
@@ -5,5 +6,5 @@
 
 r=5
 def perimetre_cercle(r):
-    return(2*3.14*r)
+    return(2*math.pi*r)
 print(perimetre_cercle(r))
